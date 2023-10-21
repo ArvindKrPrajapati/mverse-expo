57228805568db7edda12871a050f3154ee2bb5ce
ArvindKrPrajapati <arvind@earnnest.me>
2023-10-21 11:20:54 +0530

    uncommented top navigator

--- app/profile/[username].tsx
+++ app/profile/[username].tsx
@@ -10,34 +10,33 @@ const Tab = createMaterialTopTabNavigator();
 const ProfileScreen = () => {
   const colorScheme = useColorScheme();
 
-  return null
-  // return (
-  //   <Tab.Navigator
-  //     screenOptions={{
-  //       tabBarActiveTintColor: Colors[colorScheme ?? "light"].secondaryText,
-  //       tabBarInactiveTintColor: Colors[colorScheme ?? "light"].text,
-  //       tabBarLabelStyle: { fontWeight: "bold" },
-  //       tabBarScrollEnabled: true,
-  //       tabBarStyle: {
-  //         backgroundColor: Colors[colorScheme ?? "light"].background,
-  //       },
-  //       tabBarIndicatorStyle: { backgroundColor: Colors.dark.purple },
-  //       tabBarItemStyle: {
-  //         width: 120,
-  //       },
-  //       tabBarAndroidRipple: {
-  //         borderless: false,
-  //       },
-  //       swipeEnabled: true,
-  //       animationEnabled: true,
-  //     }}
-  //   >
-  //     <Tab.Screen name="About" component={AboutTab} />
-  //     <Tab.Screen name="Home" component={HomeTab} />
-  //     <Tab.Screen name="Videos" component={VideosTab} />
-  //     <Tab.Screen name="Playlist" component={PlaylistTab} />
-  //   </Tab.Navigator>
-  // );
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].secondaryText,
+        tabBarInactiveTintColor: Colors[colorScheme ?? "light"].text,
+        tabBarLabelStyle: { fontWeight: "bold" },
+        tabBarScrollEnabled: true,
+        tabBarStyle: {
+          backgroundColor: Colors[colorScheme ?? "light"].background,
+        },
+        tabBarIndicatorStyle: { backgroundColor: Colors.dark.purple },
+        tabBarItemStyle: {
+          width: 120,
+        },
+        tabBarAndroidRipple: {
+          borderless: false,
+        },
+        swipeEnabled: true,
+        animationEnabled: true,
+      }}
+    >
+      <Tab.Screen name="About" component={AboutTab} />
+      <Tab.Screen name="Home" component={HomeTab} />
+      <Tab.Screen name="Videos" component={VideosTab} />
+      <Tab.Screen name="Playlist" component={PlaylistTab} />
+    </Tab.Navigator>
+  );
 };
 
 export default ProfileScreen;
